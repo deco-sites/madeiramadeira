ae5119c3edcb0c21114bc58faf707144db3f49b2
ganobrega <gnobrega.contato@gmail.com>
2023-10-04 03:00:18 -0300

    feat: reduce import of GlobalTags

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from "$fresh/server.ts";
-import GlobalTags from "$store/components/GlobalTags.tsx";
-import Theme from "$store/sections/Theme/Theme.tsx";
+import { asset, Head } from "$fresh/runtime.ts";
+// import Theme from "$store/sections/Theme/Theme.tsx";
 
 const sw = () =>
   addEventListener("load", () =>
@@ -11,10 +11,31 @@ function App(props: AppProps) {
   return (
     <>
       {/* Include default fonts and css vars */}
-      <Theme />
+      {/* <Theme /> */}
 
       {/* Include Icons and manifest */}
-      <GlobalTags />
+      <Head>
+        {/* Enable View Transitions API */}
+        <meta name="view-transition" content="same-origin" />
+
+        {/* Tailwind v3 CSS file */}
+        <link href={asset("/styles.css")} rel="stylesheet" />
+
+        {/* Web Manifest */}
+        <link rel="manifest" href={asset("/site.webmanifest")} />
+
+        {/* Web Font */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
 
       {/* Rest of Preact tree */}
       <props.Component />

624b00e1f69c046dbee16dd6ca894ca925174fec
ganobrega <gnobrega.contato@gmail.com>
2023-10-05 09:02:27 -0300

    feat(sections): banner of 3 images inline + benefits

--- components/ui/BannerGrid.tsx
+++ components/ui/BannerGrid.tsx
@@ -1,4 +1,5 @@
 import { Picture, Source } from "apps/website/components/Picture.tsx";
+import Image from "apps/website/components/Image.tsx";
 import type { ImageWidget } from "apps/admin/widgets.ts";
 
 /**
@@ -87,50 +88,20 @@ export default function BannnerGrid(props: Props) {
   } = { ...DEFAULT_PROPS, ...props };
 
   return (
-    <section class="container w-full px-4 md:px-0 mx-auto">
-      {title &&
-        (
-          <div class="py-6 md:py-0 md:pb-[40px] flex items-center mt-6">
-            <h2 class="text-lg leading-5 font-semibold uppercase">
-              {title}
-            </h2>
-
-            <div class="bg-[#e5e5ea] h-[1px] w-full ml-4"></div>
-          </div>
-        )}
-      <div
-        class={`grid gap-4 md:gap-6 sm:grid-cols-3 max-sm:grid-rows-3`}
-      >
-        {banners.map(({ href, srcMobile, srcDesktop, alt }) => (
-          <a
-            href={href}
-            class={`overflow-hidden`}
-          >
-            <Picture>
-              <Source
-                media="(max-width: 767px)"
-                src={srcMobile}
-                width={100}
-                height={100}
-              />
-              <Source
-                media="(min-width: 768px)"
-                src={srcDesktop ? srcDesktop : srcMobile}
-                width={250}
-                height={250}
-              />
-              <img
-                class="w-full rounded-lg"
-                sizes="(max-width: 640px) 100vw, 30vw"
-                src={srcMobile}
-                alt={alt}
-                decoding="async"
-                loading="lazy"
-              />
-            </Picture>
-          </a>
-        ))}
-      </div>
+    <section class="flex flex-col md:flex-row md:py-4 justify-between gap-6 container w-full px-4 md:px-0 mx-auto">
+      {banners.map(({ href, srcMobile, srcDesktop, alt }) => (
+        <a
+          href={href}
+          class={`overflow-hidden aspect-[1/1] flex-1 rounded-lg`}
+        >
+          <Image
+            src={srcMobile}
+            width={300}
+            height={300}
+            class="mx-auto h-full w-full object-contain aspect-[1/1]"
+          />
+        </a>
+      ))}
     </section>
   );
 }
--- sections/Content/Benefits.tsx
+++ sections/Content/Benefits.tsx
@@ -6,7 +6,6 @@ export interface Props {
   title?: string;
   description?: string;
   benefits?: Array<{
-
     /**
      * @titleBy title
      */
@@ -33,39 +32,40 @@ export default function Benefits(
   return (
     <>
       <div class="w-full container px-4 py-8 flex flex-col gap-8 lg:gap-10 lg:py-10 lg:px-0">
-        <Header
+        {
+          /* <Header
           title={title}
           description={description}
           alignment="left"
-        />
+        /> */
+        }
         <div class="w-full flex justify-center">
-          <div class="flex flex-col gap-4 lg:gap-8 w-full lg:grid grid-flow-col auto-cols-fr">
+          <div class="flex flex-col gap-4 lg:gap-24 w-full lg:grid grid-flow-col auto-cols-fr">
             {benefits.map((benefit, index) => {
               return (
-                <div
-                  class={``}
-                >
-                  <div class="flex-none">
+                <div class="grid grid-rows-[6rem_3rem_1fr_1fr] text-center">
+                  <div>
                     <Image
                       src={benefit.image}
-                      style={{ aspectRatio: "108 / 150" }}
-                      width={108}
-                      height={150}
-                      class="h-full object-contain"
+                      width={96}
+                      height={96}
+                      class="mx-auto h-full object-contain aspect-[1/1]"
                     />
                   </div>
-                  <div class="flex-auto flex flex-col gap-1 lg:gap-2">
-                    <div
-                      class={`text-base lg:text-xl leading-7`}
+                  <div class="text-base font-semibold text-black">
+                    {benefit.title}
+                  </div>
+                  <p class="text-[14px] px-10">
+                    {benefit.description}
+                  </p>
+
+                  <div class="mt-1.5">
+                    <a
+                      class="inline-block border text-[#004ABE] border-[#004ABE] px-4 py-2 font-semibold rounded-sm"
+                      href={benefit.link.href}
                     >
-                      {benefit.title}
-                    </div>
-                    <p
-                      class={`text-sm leading-5`}
-                    >
-                      {benefit.description}
-                    </p>
-                    <a href={benefit.link.href}>{benefit.link.label}</a>
+                      {benefit.link.label}
+                    </a>
                   </div>
                 </div>
               );
@@ -73,7 +73,6 @@ export default function Benefits(
           </div>
         </div>
       </div>
-      )
     </>
   );
 }

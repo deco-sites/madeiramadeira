66590afd39088d1aa3bc4edd22f7820ef7f20ac5
ganobrega <gnobrega.contato@gmail.com>
2023-10-05 09:03:14 -0300

    feat: newsletter design

--- sections/Newsletter/Newsletter.tsx
+++ sections/Newsletter/Newsletter.tsx
@@ -1,127 +1,40 @@
-import Header from "$store/components/ui/SectionHeader.tsx";
-
-export interface Form {
-  placeholder?: string;
-  buttonText?: string;
-  /** @format html */
-  helpText?: string;
-}
-
-export interface Props {
-  title?: string;
-  /** @format textarea */
-  description?: string;
-  form?: Form;
-  layout?: {
-    headerFontSize?: "Large" | "Normal";
-    content?: {
-      border?: boolean;
-      alignment?: "Center" | "Left" | "Side to side";
-      bgColor?: "Normal" | "Reverse";
-    };
-  };
-}
-
-const DEFAULT_PROPS: Props = {
-  title: "",
-  description: "",
-  form: {
-    placeholder: "Digite seu email",
-    buttonText: "Inscrever",
-    helpText:
-      'Ao se inscrever, você concorda com nossa <a class="link" href="/politica-de-privacidade">Política de privacidade</a>.',
-  },
-  layout: {
-    headerFontSize: "Large",
-    content: {
-      border: false,
-      alignment: "Center",
-    },
-  },
-};
-
-export default function Newsletter(props: Props) {
-  const { title, description, form, layout } = { ...DEFAULT_PROPS, ...props };
-  const isReverse = layout?.content?.bgColor === "Reverse";
-  const bordered = Boolean(layout?.content?.border);
-
-  const headerLayout = (
-    <Header
-      title={title}
-      description={description}
-      alignment={layout?.content?.alignment === "Left" ? "left" : "center"}
-      colorReverse={isReverse}
-      fontSize={layout?.headerFontSize}
-    />
-  );
-
-  const formLayout = form && (
-    <form action="/" class="flex flex-col gap-4">
-      <div class="flex flex-col lg:flex-row gap-3">
-        <input
-          class="input input-bordered lg:w-80"
-          type="text"
-          placeholder={form.placeholder}
-        />
-        <button
-          class={`btn ${isReverse ? "btn-accent" : ""}`}
-          type="submit"
-        >
-          {form.buttonText}
-        </button>
-      </div>
-      {form.helpText && (
-        <div
-          class="text-sm"
-          dangerouslySetInnerHTML={{ __html: form.helpText }}
-        />
-      )}
-    </form>
-  );
-
-  const bgLayout = isReverse
-    ? "bg-secondary text-secondary-content"
-    : "bg-transparent";
-
+export default function Newsletter() {
   return (
-    <div
-      class={`${
-        bordered
-          ? isReverse ? "bg-secondary-content" : "bg-secondary"
-          : bgLayout
-      } ${bordered ? "p-4 lg:p-16" : "p-0"}`}
-    >
-      {(!layout?.content?.alignment ||
-        layout?.content?.alignment === "Center") && (
-        <div
-          class={`container flex flex-col rounded p-4 gap-6 lg:p-16 lg:gap-12 ${bgLayout}`}
-        >
-          {headerLayout}
-          <div class="flex justify-center">
-            {formLayout}
-          </div>
+    <div class="max-sm:p-3 mt-2">
+      <div class="bg-[#004abe] md:container rounded-lg flex flex-col rounded p-4 gap-6 sm:py-10 lg:p-16 lg:gap-12">
+        <div  class="w-11/12 xl:w-9/12 mx-auto">
+          <p class="text-center text-xl md:text-lg xl:text-xl text-white font-semibold mb-4">
+            Receba as melhores ofertas e descontos exclusivos! Cadastre seu nome e
+            e-mail aqui.
+          </p>
+          <form action="/" class="flex flex-col justify-center gap-4 px-6">
+            <div class="flex flex-col lg:flex-row gap-3">
+              <input
+                class="rounded-sm px-3 py-2 bg-white w-full md:w-full lg:w-8/12"
+                type="text"
+                placeholder="Digite o seu nome"
+              />
+              <input
+                class="rounded-sm px-3 py-2 bg-white w-full md:w-full lg:w-full"
+                type="text"
+                placeholder="Digite o seu email"
+              />
+              <button
+                class="rounded-sm bg-[#f58220] py-2 px-6 font-bold text-white"
+                type="submit"
+              >
+                Cadastrar
+              </button>
+            </div>
+            {/* {form?.helpText && (
+              <div
+                class="text-sm"
+                dangerouslySetInnerHTML={{ __html: form?.helpText }}
+              />
+            )} */}
+          </form>
         </div>
-      )}
-      {layout?.content?.alignment === "Left" && (
-        <div
-          class={`container flex flex-col rounded p-4 gap-6 lg:p-16 lg:gap-12 ${bgLayout}`}
-        >
-          {headerLayout}
-          <div class="flex justify-start">
-            {formLayout}
-          </div>
-        </div>
-      )}
-      {layout?.content?.alignment === "Side to side" && (
-        <div
-          class={`container flex flex-col rounded justify-between lg:flex-row p-4 gap-6 lg:p-16 lg:gap-12 ${bgLayout}`}
-        >
-          {headerLayout}
-          <div class="flex justify-center">
-            {formLayout}
-          </div>
-        </div>
-      )}
+      </div>
     </div>
   );
 }
